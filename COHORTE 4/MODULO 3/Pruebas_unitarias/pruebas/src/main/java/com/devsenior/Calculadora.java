f87a0bcdb4244cde670c4c41887154808381bb7f
Luis Alberto Rincon <bettokc@gmail.com>
2026-06-10 23:03:41 -0500

    Actualizacion de la clase CalculadoraTest.

--- COHORTE 4/MODULO 3/Pruebas_unitarias/pruebas/src/main/java/com/devsenior/Calculadora.java
+++ COHORTE 4/MODULO 3/Pruebas_unitarias/pruebas/src/main/java/com/devsenior/Calculadora.java
@@ -1,28 +1,33 @@
 package com.devsenior;
 
+// Clase calculadora
 public class Calculadora {
-
+    // Metodo para sumar 2 numeros enteros, retorna la suma de los enteros
     public int sumar(int a, int b) {
         return a + b;
     }
 
-    public int resta(int a, int b) {
+    // Metodo para restar 2 numeros enteros, retorna la resta de los enteros
+    public int restar(int a, int b) {
         return a - b;
     }
 
+    // Metodo para multiplicar 2 numeros enteros, retorna la multiplicacion de los
+    // enteros
     public int multiplicar(int a, int b) {
         return a * b;
     }
 
+    // Metodo para dividir 2 numeros enteros, retorna la division de los enteros
     public int dividir(int a, int b) {
-
+        // Valida si el denominador es cero, muestra una excepcion de tipo argumento
+        // ilegal
         if (b == 0) {
             throw new IllegalArgumentException("No es posible dividir por cero");
         }
+        // Si el denominador no es cero, retorna la division
+
         return a / b;
     }
 
-    
 }
-
-
--- COHORTE 4/MODULO 3/Pruebas_unitarias/pruebas/src/test/java/com/devsenior/CalculadoraTest.java
+++ COHORTE 4/MODULO 3/Pruebas_unitarias/pruebas/src/test/java/com/devsenior/CalculadoraTest.java
@@ -1,5 +1,62 @@
 package com.devsenior;
 
+// Clase de pruebas unitarias que utiliza JUnit 5 para verificar 
+// que los métodos de la clase Calculadora funcionan correctamente.
+
+// Importaciones
+// Import static permite usar lo metodos directamente
+// AssertEquals sirve para comparar 2 valores
+// AssertThrows sirve para verificar que un metodo lance una excepcion
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+// Permite marcar métodos como pruebas unitarias.
+import org.junit.jupiter.api.Test;
+
+// Declaracion de la clase 
 public class CalculadoraTest {
 
+    // Prueba de suma - @Test indica que se va a crear un metodo de prueba
+    // Los metodos de Test no deben ser privados ni estaticos y no deben devolver
+    // ningun valor
+    @Test
+    void testSuma() {
+        // Se crea la calculadora: calc
+        Calculadora calc = new Calculadora();
+        // Compara el valor establecido 5 con la suma de 2 + 3, si el resultado es 5 la
+        // prueba pasa
+        assertEquals(5, calc.sumar(2, 3));
+    }
+
+    @Test
+    void testResta() {
+        Calculadora calc = new Calculadora();
+        assertEquals(5, calc.restar(10, 5));
+    }
+
+    @Test
+    void testMultiplicar() {
+        Calculadora calc = new Calculadora();
+        assertEquals(25, calc.multiplicar(5, 5));
+    }
+
+    @Test
+    void testDividir() {
+        Calculadora calc = new Calculadora();
+        assertEquals(6, calc.dividir(12, 2));
+    }
+
+    // Prueba de division por cero
+    // Esta prueba verifica que la calculadora maneje correctamente un error.
+    @Test
+    void testDivisionPorCero() {
+        // Crear la calculadora
+        Calculadora calc = new Calculadora();
+        // Capturar la excepcion, se valida que calc.dividir lance
+        // IllegalArgumentException
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            calc.dividir(6, 0);
+        });
+        // Compara valor esperado con valor obtenido
+        assertEquals("No es posible dividir por cero", exception.getMessage());
+    }
 }
